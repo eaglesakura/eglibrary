39b00401dfcd13e2695d7558381e869e6fedb404
eaglesakura <eagle.sakura@gmail.com>
2012-04-18 17:56:50 +0900

    コメント追加

--- eglibrary-android-api8/src/com/eaglesakura/lib/android/game/resource/IRawResource.java
+++ eglibrary-android-api8/src/com/eaglesakura/lib/android/game/resource/IRawResource.java
@@ -1,7 +1,10 @@
 package com.eaglesakura.lib.android.game.resource;
 
 /**
- * 
+ * {@link System#gc()}による管理ができない大規模リソースを保持する。
+ * OpenGL ESのテクスチャ、Bitmapクラス等の管理を行う。
+ * 指定した{@link GarbageCollector}で一元管理を行い、適当なタイミングでリソースの一括解放を行う。
+ * 管理す忘れたリソースの解放ミスを防ぐ。
  * @author TAKESHI YAMASHITA
  *
  */
