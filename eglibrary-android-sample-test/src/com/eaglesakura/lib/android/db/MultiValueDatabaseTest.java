3d6040ced0fccd82c9b998fbd28104f0fb7f63dc
eaglesakura <yamashitatakeshi@ip31-pc.topgate.lan>
2012-07-20 18:58:06 +0900

    リストアップ用のテストを追加

--- eglibrary-android-sample-test/src/com/eaglesakura/lib/android/db/MultiValueDatabaseTest.java
+++ eglibrary-android-sample-test/src/com/eaglesakura/lib/android/db/MultiValueDatabaseTest.java
@@ -2,12 +2,14 @@ package com.eaglesakura.lib.android.db;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import android.os.Environment;
 import android.test.AndroidTestCase;
 
 import com.eaglesakura.lib.android.db.MultiValueDatabase.Column;
+import com.eaglesakura.lib.android.db.MultiValueDatabase.Data;
 import com.eaglesakura.lib.android.game.util.LogUtil;
 
 public class MultiValueDatabaseTest extends AndroidTestCase {
@@ -22,6 +24,7 @@ public class MultiValueDatabaseTest extends AndroidTestCase {
 
         valueList.setPrimary(new Column(DBValueType.Text, "_shf"));
         valueList.addColmn(new Column(DBValueType.Text, "_bh"));
+        valueList.addColmn(new Column(DBValueType.Text, "_cmt"));
 
         DB_FILE.delete();
     }
@@ -92,6 +95,42 @@ public class MultiValueDatabaseTest extends AndroidTestCase {
             MultiValueDatabase.Data data = db.getOrNull("shelf");
             assertNotNull(data);
             assertEquals(data.getText("_bh", null), "bookhash-update");
+        }
+    }
+
+    public void listTest() {
+
+        MultiValueDatabase db = new MultiValueDatabase(getContext(), DB_FILE, DBType.ReadWrite, valueList);
+
+        // データを新規追加する
+        {
+            {
+                Map<String, Object> values = new HashMap<String, Object>();
+                values.put("_shf", "shelf0");
+                values.put("_bh", "bookhash");
+                values.put("_cmt", "こめんと");
+                assertTrue(db.insertOrUpdate(values));
+            }
+            {
+                Map<String, Object> values = new HashMap<String, Object>();
+                values.put("_shf", "shelf1");
+                values.put("_bh", "bookhash");
+                values.put("_cmt", "こめんと2");
+                assertTrue(db.insertOrUpdate(values));
+            }
+            {
+                Map<String, Object> values = new HashMap<String, Object>();
+                values.put("_shf", "shelf2");
+                values.put("_bh", "bookhash");
+                values.put("_cmt", "こめんと4");
+                assertTrue(db.insertOrUpdate(values));
+            }
+        }
+
+        List<Data> list = db.list("_bh", "bookhash");
+        assertTrue(list.size() == 3);
+        for (Data d : list) {
+            assertEquals(d.getText("_bh", null), "bookhash");
         }
     }
 }
