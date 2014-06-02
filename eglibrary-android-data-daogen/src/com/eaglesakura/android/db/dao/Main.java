de63bab87023177959e60e35d8cf615bfee9578a
eaglesakura <eagle.sakura@gmail.com>
2014-06-03 08:47:36 +0900

    プロジェクト名を変更、KVS用データを追加

---
+++ eglibrary-android-data-daogen/src/com/eaglesakura/android/db/dao/Main.java
@@ -0,0 +1,59 @@
+package com.eaglesakura.android.db.dao;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class Main {
+
+    private static final String PACKAGE_NAME = "com.eaglesakura.andriders.dao";
+
+    private static final String OUTPUT_PATH = "../eglibrary-android-data/dao";
+
+    public static void main(String[] args) {
+        try {
+            genTextKeyValueStoreDao();
+            genBlobKeyValueStoreDao();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * シンプルなKey-Value-Storeの内容
+     * @throws Exception
+     */
+    static void genTextKeyValueStoreDao() throws Exception {
+        Schema scheme = new Schema(0x01, PACKAGE_NAME + ".tkvs");
+        {
+            Entity entity = scheme.addEntity("DbKeyValueData");
+            // uniqueId
+            entity.addStringProperty("key").notNull().unique().primaryKey();
+            // value
+            entity.addStringProperty("value");
+            // 更新日時
+            entity.addDateProperty("date").notNull();
+        }
+
+        new DaoGenerator().generateAll(scheme, OUTPUT_PATH);
+    }
+
+    /**
+     * シンプルなKey-Value-Storeの内容
+     * @throws Exception
+     */
+    static void genBlobKeyValueStoreDao() throws Exception {
+        Schema scheme = new Schema(0x01, PACKAGE_NAME + ".tkvs");
+        {
+            Entity entity = scheme.addEntity("DbKeyValueData");
+            // uniqueId
+            entity.addStringProperty("key").notNull().unique().primaryKey();
+            // value
+            entity.addByteArrayProperty("value");
+            // 更新日時
+            entity.addDateProperty("date").notNull();
+        }
+
+        new DaoGenerator().generateAll(scheme, OUTPUT_PATH);
+    }
+}
---
+++ eglibrary-android-data/src/com/eaglesakura/android/json/JSON.java
@@ -0,0 +1,80 @@
+package com.eaglesakura.android.json;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.eaglesakura.util.LogUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSON {
+    /**
+     * クラスを文字列へエンコードする
+     * @param obj
+     * @return
+     */
+    public static void encode(OutputStream os, Object obj) throws JsonProcessingException, IOException {
+        new ObjectMapper().writeValue(os, obj);
+    }
+
+    /**
+     * クラスを文字列へエンコードする
+     * @param obj
+     * @return
+     */
+    public static String encode(Object obj) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(obj);
+    }
+
+    /**
+     * クラスを文字列へエンコードする
+     * @param obj
+     * @return
+     */
+    public static String encodeOrNull(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            LogUtil.log(e);
+
+            return null;
+        }
+    }
+
+    /**
+     * 文字列をクラスへデコードする
+     */
+    public static <T> T decode(String json, Class<T> clazz) throws JsonProcessingException, IOException {
+        return new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).readValue(json, clazz);
+    }
+
+    /**
+     * 文字列をクラスへデコードする
+     */
+    public static <T> T decodeOrNull(String json, Class<T> clazz) {
+        try {
+            return new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).readValue(json, clazz);
+        } catch (Exception e) {
+            //            LogUtil.log(e);
+            return null;
+        }
+    }
+
+    /**
+     * 文字列をクラスへデコードする
+     */
+    public static <T> T decode(InputStream is, Class<T> clazz) throws JsonProcessingException, IOException {
+        return new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).readValue(is, clazz);
+    }
+
+    /**
+     * JSONモデルを送信用byte[]に変換する
+     * @param model
+     * @return
+     */
+    public static byte[] model2bytes(Object model) {
+        return encodeOrNull(model).getBytes();
+    }
+}
