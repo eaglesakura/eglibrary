1cbe6410d332c11835d8ac7d4657998e618d4021
eaglesakura <eagle.sakura@gmail.com>
2014-06-19 12:23:14 +0900

    ビルド構成を別プロジェクト依存になるように修正

--- eglibrary-android-connect/src/com/eaglesakura/android/bluetooth/ui/BluetoothAdapterInitializeFragment.java
+++ eglibrary-android-connect/src/com/eaglesakura/android/bluetooth/ui/BluetoothAdapterInitializeFragment.java
@@ -14,7 +14,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 
-import com.eaglesakura.R;
+import com.eaglesakura.android.connect.R;
 import com.eaglesakura.android.bluetooth.BluetoothUtil;
 import com.eaglesakura.android.util.FragmentUtil;
 
