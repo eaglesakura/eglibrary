7ca58f7eaf5de72ce9365e76a83beedb3f335f11
eaglesakura <eagle.sakura@gmail.com>
2014-07-09 16:36:02 +0900

    DeviceScannerにRSSI等の追加情報をキャッシュ出来る機構を追加

--- eglibrary-android-java-wrapper/src/com/eaglesakura/android/bluetooth/BluetoothDeviceScanner.java
+++ eglibrary-android-java-wrapper/src/com/eaglesakura/android/bluetooth/BluetoothDeviceScanner.java
@@ -15,6 +15,8 @@ import com.eaglesakura.util.LogUtil;
 import com.eaglesakura.util.Util;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -26,13 +28,16 @@ public class BluetoothDeviceScanner {
     BluetoothAdapter bluetoothAdapter;
 
     /**
-     * 発見したデバイス
+     * 発見したデバイスのキャッシュ
      */
-    List<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+    List<BluetoothDeviceCache> deviceCaches = new ArrayList<BluetoothDeviceCache>();
 
     /**
-     * 
+     * 指定時間以上前に発見されたデバイスはclean対象となる
+     * デフォルト時間は要調整
      */
+    long existCacheTimeMs = 1000 * 15;
+
     final Context context;
 
     final Runnable scanStopRunnable = new Runnable() {
@@ -61,12 +66,58 @@ public class BluetoothDeviceScanner {
     private class LeScanCallbackImpl implements LeScanCallback {
         @Override
         public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-            if (Util.addUnique(devices, device)) {
-                // le connected
-                listener.onDeviceFound(BluetoothDeviceScanner.this, device);
+            BluetoothDeviceCache cache = getDeviceCache(device);
+            if (cache == null) {
+                // キャッシュがないので、新規ヒットしたデバイスである
+                cache = new BluetoothDeviceCache(device, rssi, scanRecord);
+
+                // キャッシュを追加する
+                deviceCaches.add(cache);
+
+                // コールバック呼び出し
+                listener.onDeviceFound(BluetoothDeviceScanner.this, cache);
+            } else {
+                // キャッシュを更新する
+                cache.sync(device, rssi, scanRecord);
+
+                // コールバック呼び出し
+                listener.onDeviceUpdated(BluetoothDeviceScanner.this, cache);
             }
         }
-    };
+    }
+
+    /**
+     * 保持しているキャッシュをチェックし、不要なものを削除する
+     */
+    public void cleanDeviceCaches() {
+        Iterator<BluetoothDeviceCache> iterator = deviceCaches.iterator();
+        while (iterator.hasNext()) {
+            BluetoothDeviceCache deviceCache = iterator.next();
+            if (!deviceCache.exist()) {
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * キャッシュ時刻が有効なキャッシュ一覧を取得する
+     */
+    private BluetoothDeviceCache getDeviceCache(BluetoothDevice device) {
+
+        // 不要なデータを削除する
+        cleanDeviceCaches();
+
+        // キャッシュチェック
+        for (BluetoothDeviceCache cache : deviceCaches) {
+            if (cache.address.equals(device.getAddress())) {
+                // 一致した
+                return cache;
+            }
+        }
+
+        // キャッシュヒットしなかった
+        return null;
+    }
 
     /**
      * Bluetoothデバイス検索
@@ -81,8 +132,23 @@ public class BluetoothDeviceScanner {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
                 // リストに追加
-                if (device != null && Util.addUnique(devices, device)) {
-                    listener.onDeviceFound(BluetoothDeviceScanner.this, device);
+                if (device != null) {
+                    BluetoothDeviceCache cache = getDeviceCache(device);
+
+                    // 電波強度を取得
+                    int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, RSSI_UNKNOWN);
+
+                    if (cache == null) {
+                        // キャッシュが見つからないので、新規にヒットしたデバイス
+                        cache = new BluetoothDeviceCache(device, rssi, null);
+                        deviceCaches.add(cache);
+
+                        listener.onDeviceFound(BluetoothDeviceScanner.this, cache);
+                    } else {
+                        // キャッシュヒットしたので、更新する
+                        cache.sync(device, rssi, null);
+                        listener.onDeviceUpdated(BluetoothDeviceScanner.this, cache);
+                    }
                 }
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 // スキャンを停止させる
@@ -99,7 +165,6 @@ public class BluetoothDeviceScanner {
 
     /**
      * デバイススキャンを行う
-     * @param context
      */
     @SuppressLint("InlinedApi")
     public BluetoothDeviceScanner(Context context, BluetoothDeviceType mode) {
@@ -119,18 +184,17 @@ public class BluetoothDeviceScanner {
 
     /**
      * リスナ指定を行う
-     * @param listener
      */
     public void setListener(DeviceScanListener listener) {
         this.listener = listener;
     }
 
     /**
-     * スキャンしたデバイスを取得する
-     * @return
+     * キャッシュが有効な時間を指定する
+     * @param existCacheTimeMs キャッシュが有効な時間(ミリ秒)
      */
-    public List<BluetoothDevice> getBluetoothDevices() {
-        return devices;
+    public void setExistCacheTimeMs(long existCacheTimeMs) {
+        this.existCacheTimeMs = existCacheTimeMs;
     }
 
     /**
@@ -179,15 +243,129 @@ public class BluetoothDeviceScanner {
 
     public interface DeviceScanListener {
         /**
-         * デバイスを見つけた
-         * @param device
+         * デバイスを見つけた場合に呼び出される。
+         *
+         * @param self   コールバック呼び出し元
+         * @param device 発見したデバイス
          */
-        void onDeviceFound(BluetoothDeviceScanner self, BluetoothDevice device);
+        void onDeviceFound(BluetoothDeviceScanner self, BluetoothDeviceCache device);
+
+        /**
+         * デバイス情報が更新された場合に呼び出される。
+         *
+         * @param self   　コールバック呼び出し元
+         * @param device 更新されたデバイス
+         */
+        void onDeviceUpdated(BluetoothDeviceScanner self, BluetoothDeviceCache device);
 
         /**
          * スキャンがタイムアウトした
-         * @param self
+         *
+         * @param self コールバック呼び出し元
          */
         void onScanTimeout(BluetoothDeviceScanner self);
+    }
+
+    /**
+     * 不明なRSSI値
+     */
+    public static final short RSSI_UNKNOWN = (short) 0xFEFE;
+
+    /**
+     * 発見したデバイスのキャッシュを行う
+     */
+    public class BluetoothDeviceCache {
+        /**
+         * デバイス
+         */
+        BluetoothDevice device;
+
+        /**
+         * 電波強度
+         */
+        int rssi = RSSI_UNKNOWN;
+
+
+        /**
+         * record
+         */
+        byte[] scanRecord;
+
+        /**
+         * 発見された時刻
+         */
+        final Date connectedDate;
+
+        /**
+         * 比較用のアドレス
+         */
+        final String address;
+
+        private BluetoothDeviceCache(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            this.device = device;
+            this.rssi = rssi;
+            this.scanRecord = scanRecord;
+            this.address = device.getAddress();
+            this.connectedDate = new Date();
+        }
+
+        /**
+         * キャッシュが有効であればtrue
+         */
+        public boolean exist() {
+            return (System.currentTimeMillis() - connectedDate.getTime()) < existCacheTimeMs;
+        }
+
+        /**
+         * デバイスを取得する
+         */
+        public BluetoothDevice getDevice() {
+            return device;
+        }
+
+        /**
+         * 電波強度を取得する
+         */
+        public int getRssi() {
+            return rssi;
+        }
+
+        /**
+         * recordを取得する
+         */
+        public byte[] getScanRecord() {
+            return scanRecord;
+        }
+
+        /**
+         * 同期を行う
+         */
+        private void sync(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            assert device != null;
+            assert device.getAddress().equals(address);
+
+            this.device = device;
+            this.scanRecord = scanRecord;
+            this.rssi = rssi;
+            this.connectedDate.setTime(System.currentTimeMillis());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            BluetoothDeviceCache that = (BluetoothDeviceCache) o;
+
+            if (address != null ? !address.equals(that.address) : that.address != null)
+                return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return address != null ? address.hashCode() : 0;
+        }
     }
 }
