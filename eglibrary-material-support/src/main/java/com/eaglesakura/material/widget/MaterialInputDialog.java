674dc8b051e573b61c91d52dbd9c3fc88dc5821b
eaglesakura <eagle.sakura@gmail.com>
2014-11-13 00:22:32 +0900

    入力バリデーション機能を追加

--- eglibrary-material-support/src/main/java/com/eaglesakura/material/widget/MaterialInputDialog.java
+++ eglibrary-material-support/src/main/java/com/eaglesakura/material/widget/MaterialInputDialog.java
@@ -2,11 +2,14 @@ package com.eaglesakura.material.widget;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.eaglesakura.material.R;
+import com.eaglesakura.util.StringUtil;
 
 /**
  * 入力用ダイアログを構築する
@@ -19,6 +22,8 @@ public abstract class MaterialInputDialog extends MaterialAlertDialog {
 
     private TextView fooderText;
 
+    private TextView validateText;
+
     public MaterialInputDialog(Context context) {
         super(context);
 
@@ -27,6 +32,33 @@ public abstract class MaterialInputDialog extends MaterialAlertDialog {
         inputText = (EditText) content.findViewById(R.id.EsMaterial_Dialog_Input_EditText);
         headerText = (TextView) content.findViewById(R.id.EsMaterial_Dialog_Input_Header);
         fooderText = (TextView) content.findViewById(R.id.EsMaterial_Dialog_Input_Fooder);
+        validateText = (TextView) content.findViewById(R.id.EsMaterial_Dialog_Input_ValidateHint);
+
+        inputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (validateInputText(inputText, validateText)) {
+                    // OKを表示する
+                    findViewById(R.id.EsMaterial_Dialog_BasicButtons_Positive).setEnabled(true);
+                    validateText.setVisibility(View.GONE);
+                } else {
+                    // OKをdisable
+                    findViewById(R.id.EsMaterial_Dialog_BasicButtons_Positive).setEnabled(false);
+                    validateText.setVisibility(View.VISIBLE);
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         setPositiveButton(R.string.EsMaterial_Dialog_OK, new OnClickListener() {
             @Override
@@ -43,6 +75,17 @@ public abstract class MaterialInputDialog extends MaterialAlertDialog {
     public void show() {
         onInitializeViews(headerText, inputText, fooderText);
         super.show();
+    }
+
+    /**
+     * 入力された内容がcommit可能であればtrueを返す
+     *
+     * @param text
+     * @param validateHintText
+     * @return
+     */
+    protected boolean validateInputText(EditText text, TextView validateHintText) {
+        return !StringUtil.isEmpty(text.getText().toString());
     }
 
     /**
