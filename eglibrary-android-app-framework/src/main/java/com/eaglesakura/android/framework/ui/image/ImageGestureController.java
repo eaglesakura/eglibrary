7df7a65794e312b9b350555bf481c33c006e24e8
eaglesakura <eagle.sakura@gmail.com>
2014-12-24 10:24:14 +0900

    カスタマイズしたImageView用のUtilを追加

---
+++ eglibrary-android-app-framework/src/main/java/com/eaglesakura/android/framework/ui/image/ImageGestureController.java
@@ -0,0 +1,235 @@
+package com.eaglesakura.android.framework.ui.image;
+
+import android.content.Context;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+
+import com.eaglesakura.android.graphics.ImageCorrector;
+import com.eaglesakura.android.thread.HandlerLoopController;
+import com.eaglesakura.android.thread.UIHandler;
+import com.eaglesakura.math.MathUtil;
+import com.eaglesakura.math.Vector2;
+import com.eaglesakura.util.LogUtil;
+
+/**
+ * 一枚絵を基本として、下記の機能を扱うベースを対応する。
+ * <p/>
+ * これは操作系等を統一しているのみで、レンダリングは無関係となる。
+ */
+public class ImageGestureController implements View.OnTouchListener {
+    /**
+     * スケーリングの入力中だったらtrue
+     */
+    private boolean scaleInput = false;
+
+    private boolean touchNow = false;
+
+    private final ImageCorrector imageCorrector = new ImageCorrector();
+
+    private final Context context;
+
+    private GestureDetector touchDetector;
+
+    private ScaleGestureDetector scaleGestureDetector;
+
+    private Handler handler = UIHandler.getInstance();
+
+    private HandlerLoopController loopController;
+
+    /**
+     * 浮動小数計算で認める丸め誤差
+     */
+    private final float FLOAT_CALC_ROUND = 0.0000001f;
+
+    private float maxScaling = 3.0f;
+
+    private float scaleEffectUpBound = 1.15f;
+
+    Vector2 scaleCenter = new Vector2();
+
+
+    /**
+     * 画像移動のベクトル
+     */
+    Vector2 imageMoveVector = new Vector2();
+
+
+    public ImageGestureController(Context context) {
+        this.context = context.getApplicationContext();
+    }
+
+    public ImageCorrector getImageCorrector() {
+        return imageCorrector;
+    }
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void initialize() {
+        touchDetector = new GestureDetector(context, singleTapListener);
+        touchDetector.setOnDoubleTapListener(doubleTapListener);
+
+        scaleGestureDetector = new ScaleGestureDetector(context, scaleGestureListener);
+    }
+
+    /**
+     * エフェクト実行が必要であればtrue
+     *
+     * @return
+     */
+    public boolean hasEffectUpdateRequest() {
+        if (imageMoveVector.length() > 0.0001) {
+            return true;
+        }
+
+        // 等倍よりも小さかったら拡大処理を行う
+        if (imageCorrector.getPixelScale() < 1.0 || imageCorrector.getPixelScale() > maxScaling) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 移動を行う
+     */
+    public void updateEffect() {
+
+        if (!scaleInput) {
+            // スケーリング入力していなかったら
+            if (imageCorrector.getPixelScale() < 1.0f) {
+                imageCorrector.scale(scaleCenter.x, scaleCenter.y, scaleEffectUpBound);
+            }
+
+            // 誤差が一定以内だったらフィットさせる
+            if (Math.abs(1.0f - imageCorrector.getPixelScale()) < FLOAT_CALC_ROUND) {
+                imageCorrector.scale(1.0f / imageCorrector.getPixelScale());
+            }
+        }
+
+
+        // 位置補正
+        if (!touchNow && imageCorrector.getPixelScale() >= 1.0f) {
+            final float CENTER_X = imageCorrector.getRenderAreaCenterX();
+            final float CENTER_Y = imageCorrector.getRenderAreaCenterY();
+
+            float boundMoveSpeed = imageCorrector.getRenderAreaWidth() / 15;
+
+            if (imageCorrector.getImageAreaLeft() > CENTER_X) {
+                imageCorrector.moveToTargetLeft(CENTER_X, boundMoveSpeed);
+            }
+            if (imageCorrector.getImageAreaRight() < CENTER_X) {
+                imageCorrector.moveToTargetRight(CENTER_X, boundMoveSpeed);
+            }
+
+            if (imageCorrector.getImageAreaTop() > CENTER_Y) {
+                imageCorrector.moveToTargetTop(CENTER_X, boundMoveSpeed);
+            }
+            if (imageCorrector.getImageAreaBottom() < CENTER_Y) {
+                imageCorrector.moveToTargetBottom(CENTER_X, boundMoveSpeed);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        touchDetector.onTouchEvent(event);
+        scaleGestureDetector.onTouchEvent(event);
+        if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+            scaleInput = false;
+            touchNow = false;
+        } else {
+            touchNow = true;
+        }
+        return true;
+    }
+
+    private final GestureDetector.OnGestureListener singleTapListener = new GestureDetector.OnGestureListener() {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            if (scaleInput) {
+                return false;
+            }
+
+            imageCorrector.offset(-distanceX, -distanceY);
+
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (scaleInput) {
+                return false;
+            }
+
+            LogUtil.log("onFling(%f, %f)", velocityX, velocityY);
+//            imageMoveVector.set(velocityX, velocityY);
+            return true;
+        }
+    };
+
+    private final GestureDetector.OnDoubleTapListener doubleTapListener = new GestureDetector.OnDoubleTapListener() {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            return false;
+        }
+    };
+
+    private final ScaleGestureDetector.OnScaleGestureListener scaleGestureListener = new ScaleGestureDetector.OnScaleGestureListener() {
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            scaleInput = true;
+            scaleCenter.set(detector.getFocusX(), detector.getFocusY());
+            return true;
+        }
+
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float factor = detector.getScaleFactor();
+            factor = MathUtil.minmax(0.8f, 1.5f, factor);   // 一瞬で多大な影響を受けないように範囲制限
+            imageCorrector.scale(scaleCenter.x, scaleCenter.y, factor);
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector detector) {
+            scaleInput = false;
+        }
+    };
+}
---
+++ eglibrary-android-java-core/src/main/java/com/eaglesakura/android/graphics/VirtualDisplay.java
@@ -0,0 +1,328 @@
+package com.eaglesakura.android.graphics;
+
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import com.eaglesakura.android.util.AndroidMathUtil;
+import com.eaglesakura.math.Vector2;
+
+/**
+ * ディスプレイサイズに関わる補正を行う。<BR>
+ * <BR>
+ * 基本的に、画面はスケーリング＆センタリングされる。<BR>
+ * 基本となる仮想ディスプレイサイズを指定し、そのアスペクト比を保持して実際のディスプレイサイズが決定される。<BR>
+ * アスペクト比は可能な限り保持するが、端数の関係で完全一致は諦めること。<BR>
+ * 実際の描画を行うための解像度は必ず偶数になる。<BR>
+ * 全ての入力が偶数である場合、上下の隙間のピクセル数は必ず一致する。<BR>
+ * 
+ * @author TAKESHI YAMASHITA
+ * 
+ */
+public class VirtualDisplay {
+    /**
+     * 実際のディスプレイサイズ
+     */
+    Vector2 realDisplaySize = new Vector2(720, 1280);
+
+    /**
+     * 仮想ディスプレイサイズ
+     */
+    Vector2 virtualDisplaySize = new Vector2(480, 800);
+
+    /**
+     * 仮想ディスプレイの実描画位置
+     */
+    RectF drawingArea = new RectF(0, 0, 1, 1);
+
+    /**
+     * サイズのスケーリング値。
+     */
+    float scaling = 1.0f;
+
+    /**
+     * 仮想ディスプレイのフィットタイプ。
+     * @author TAKESHI YAMASHITA
+     *
+     */
+    public enum FitType {
+        /**
+         * 縦横を自動で判断する
+         */
+        AUTO,
+
+        /**
+         * 水平方向をフィットさせる
+         */
+        HORIZONTAL,
+
+        /**
+         * 縦方向をフィットさせる
+         */
+        VERTICAL,
+    }
+
+    /**
+     * ディスプレイサイズの補正を行う。
+     */
+    public VirtualDisplay() {
+        setRealDisplaySize(720, 1280);
+        setVirtualDisplaySize(480, 800);
+    }
+
+    /**
+     * 実ディスプレイサイズを設定する。
+     * 
+     * @param width 幅
+     * @param height 高さ
+     */
+    public void setRealDisplaySize(float width, float height) {
+        realDisplaySize.set(width, height);
+    }
+
+    /**
+     * 仮想ディスプレイのサイズを指定する。
+     * 自動的に全画面が収まるようにフィットさせる。
+     * @param width 仮想ディスプレイ幅
+     * @param height 仮想ディスプレイ高さ
+     */
+    public void setVirtualDisplaySize(float width, float height) {
+        setVirtualDisplaySize(width, height, FitType.AUTO);
+    }
+
+    /**
+     * 仮想ディスプレイサイズを設定する。
+     * 
+     * @param width 仮想ディスプレイ幅
+     * @param height 仮想ディスプレイ高さ
+     * @param type 画面をフィットさせる方法
+     */
+    public void setVirtualDisplaySize(float width, float height, FitType type) {
+        virtualDisplaySize.set(width, height);
+
+        //! 縦横比を計算
+        final float mulX = realDisplaySize.x / width;
+        final float mulY = realDisplaySize.y / height;
+
+        //! スケーリング値が小さい方にあわせて、描画先を設定する。
+        if (type == FitType.AUTO) {
+            scaling = Math.min(mulX, mulY);
+        } else if (type == FitType.VERTICAL) {
+            scaling = mulY;
+        } else if (type == FitType.HORIZONTAL) {
+            scaling = mulX;
+        }
+        drawingArea.set(0, 0, virtualDisplaySize.x * scaling, virtualDisplaySize.y * scaling);
+        AndroidMathUtil.round(drawingArea);
+
+        //! ジャストフィット用の補正を行う。
+        if (scaling == mulX && drawingArea.width() != realDisplaySize.x) {
+            drawingArea.right = realDisplaySize.x;
+        }
+        if (scaling == mulY && drawingArea.height() != realDisplaySize.y) {
+            drawingArea.bottom = realDisplaySize.y;
+        }
+
+        //! 描画位置をセンタリングする
+        int offsetX = (int) (realDisplaySize.x - drawingArea.width());
+        int offsetY = (int) (realDisplaySize.y - drawingArea.height());
+        drawingArea.offsetTo(offsetX / 2, offsetY / 2);
+        AndroidMathUtil.round(drawingArea);
+
+        //! 幅が偶数じゃない場合、丸めを行う。
+        if (((int) drawingArea.width()) % 2 != 0) {
+            drawingArea.right += 1;
+        }
+        //! 高さが偶数じゃない場合、丸めを行う。
+        if (((int) drawingArea.height()) % 2 != 0) {
+            drawingArea.bottom += 1;
+        }
+    }
+
+    /**
+     * 実際の描画先の座標を取得する。
+     * 
+     * @param result 結果を格納するオブジェクト。この参照が戻り値となる。
+     * @return 実際の描画先の座標
+     */
+    public RectF getDrawingArea(RectF result) {
+        result.set(drawingArea);
+        return result;
+    }
+
+    /**
+     * 実際の描画先座標を取得する。
+     * 
+     * @param result 結果を格納するオブジェクト。この参照が戻り値となる。
+     * @return 実際の描画先の座標
+     */
+    public Rect getDrawingArea(Rect result) {
+        result.set((int) drawingArea.left, (int) drawingArea.top, (int) drawingArea.right, (int) drawingArea.bottom);
+        return result;
+    }
+
+    /**
+     * 描画エリアの幅を取得する
+     * @return 描画エリアの幅
+     */
+    public int getDrawingAreaWidth() {
+        return (int) drawingArea.width();
+    }
+
+    /**
+     * 描画エリアの高さを取得する
+     * @return 描画エリアの高さ
+     */
+    public int getDrawingAreaHeight() {
+        return (int) drawingArea.height();
+    }
+
+    /**
+     * 実際の物理的なディスプレイサイズを取得する。
+     * 
+     * @param result 結果を格納するオブジェクト
+     * @return resultと同じ参照
+     */
+    public Vector2 getRealDisplaySize(Vector2 result) {
+        result.set(realDisplaySize);
+        return result;
+    }
+
+    /**
+     * 仮想ディスプレイサイズを取得する。
+     * 
+     * @param result 結果を格納するオブジェクト
+     * @return resultと同じ参照
+     */
+    public Vector2 getVirtualDisplaySize(Vector2 result) {
+        result.set(virtualDisplaySize);
+        return result;
+    }
+
+    /**
+     * 仮想ディスプレイに対して、実ディスプレイが何倍あるかのスケーリング値を取得する。<BR>
+     * 仮想ディスプレイのほうが小さい場合、 > 1.0f<BR>
+     * 実ディスプレイのほうが小さい場合、 < 1.0f<BR>
+     * となる。
+     * 
+     * @return　デバイスのスケーリング率
+     */
+    public float getDeviceScaling() {
+        return scaling;
+    }
+
+    /**
+     * 仮想ディスプレイの横サイズの中央を取得する。
+     * @return 仮想ディスプレイのX中央値
+     */
+    public int getVirtualDisplayCenterX() {
+        return (int) virtualDisplaySize.x / 2;
+    }
+
+    /**
+     * 仮想ディスプレイの縦サイズの中央を取得する。
+     * @return 仮想ディスプレイのX中央値
+     */
+    public int getVirtualDisplayCenterY() {
+        return (int) virtualDisplaySize.y / 2;
+    }
+
+    /**
+     * 仮想ディスプレイの横サイズを取得する。
+     * @return 仮想ディスプレイの幅
+     */
+    public int getVirtualDisplayWidth() {
+        return (int) virtualDisplaySize.x;
+    }
+
+    /**
+     * 仮想ディスプレイの縦サイズを取得する
+     * @return 仮想ディスプレイの高さ
+     */
+    public int getVirtualDisplayHeight() {
+        return (int) virtualDisplaySize.y;
+    }
+
+    /**
+     * 指定した座標が画面外だったらtrueを返す。
+     * 
+     * @param realPos 実際のデバイス座標
+     * @return 画面外だったらtrue
+     */
+    public boolean isOutsideReal(Vector2 realPos) {
+        return drawingArea.contains(realPos.x, realPos.y);
+    }
+
+    /**
+     * 指定した座標が画面外だったらtrueを返す。
+     * 
+     * @param left X座標
+     * @param top Y座標
+     * @param width 幅
+     * @param height 高さ
+     * @return 外側だったらtrue
+     */
+    public boolean isOutsideReal(int left, int top, int width, int height) {
+        return !drawingArea.contains(left, top, left + width, top + height);
+    }
+
+    /**
+     * 仮想ディスプレイの外だったらtrueを返す
+     * 
+     * @param area
+     * @return
+     */
+    public boolean isOutsideVirtual(int left, int top, int width, int height) {
+        final int right = (left + width);
+        final int bottom = (top + height);
+
+        //! 右側が0を下回っている、左側が画面サイズよりも大きい、下が画面から見切れている、上が画面から見切れている、いずれかがヒット
+        return right < 0 || left > (int) virtualDisplaySize.x || bottom < 0 || top > (int) virtualDisplaySize.y;
+    }
+
+    /**
+     * 実際のピクセル位置を仮想ディスプレイ位置に変換する。
+     * 
+     * @param realPos
+     *            ピクセル単位の位置
+     * @param result
+     *            結果を格納する参照。realPosと同じオブジェクトで問題ない。
+     * @return
+     */
+    public Vector2 projectionPixelPosition(Vector2 realPos, Vector2 result) {
+        result.set((realPos.x - drawingArea.left) / scaling, (realPos.y - drawingArea.top) / scaling);
+        return result;
+    }
+
+    /**
+     * 実際のピクセル位置を正規化座標系に変換する。 GL正規化座標系のため、下側が0.0、上側が1.0となる。
+     * 
+     * @param realPos
+     *            ピクセル単位の位置
+     * @param result
+     *            結果を格納する参照。realPosと同じオブジェクトで問題ない。
+     * @return
+     */
+    public Vector2 projectionNormalizePosition(Vector2 realPos, Vector2 result) {
+        projectionPixelPosition(realPos, result);
+        result.x /= virtualDisplaySize.x;
+        result.y /= virtualDisplaySize.y;
+
+        result.y = 1.0f - result.y;
+        return result;
+    }
+
+    /**
+     * 実際のピクセル位置を正規化座標系に変換する。 
+     * この値は、仮想ディスプレイに対するUVとして動作する。
+     * 
+     * @param realPos ピクセル単位の位置
+     * @param result 結果を格納する参照。realPosと同じオブジェクトで問題ない。
+     * @return
+     */
+    public Vector2 projectionNormalizePosition2D(Vector2 realPos, Vector2 result) {
+        projectionPixelPosition(realPos, result);
+        result.x /= virtualDisplaySize.x;
+        result.y /= virtualDisplaySize.y;
+        return result;
+    }
+}
