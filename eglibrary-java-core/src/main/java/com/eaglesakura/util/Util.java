9a2f9b87c7d644624a1b4936f9cc82af0dbc21fb
eaglesakura <eagle.sakura@gmail.com>
2015-02-11 20:15:29 +0900

    Date用Utilメソッドを追加

--- eglibrary-java-core/src/main/java/com/eaglesakura/util/Util.java
+++ eglibrary-java-core/src/main/java/com/eaglesakura/util/Util.java
@@ -26,6 +26,29 @@ public class Util {
     }
 
     /**
+     * 指定日の開始時刻を取得する
+     *
+     * @param date
+     * @return
+     */
+    public static Date getDateStart(Date date) {
+        long oneDay = 1000 * 60 * 24;
+        long now = date.getTime();
+
+        return new Date(now / oneDay * oneDay);
+    }
+
+    /**
+     * 指定日の終了時刻を取得する
+     *
+     * @param date
+     * @return
+     */
+    public static Date getDateEnd(Date date) {
+        return new Date(getDateStart(date).getTime() + (1000 * 60 * 24) - 1);
+    }
+
+    /**
      * 今日の0時0分を取得する
      */
     public static Date getTodayStart() {
