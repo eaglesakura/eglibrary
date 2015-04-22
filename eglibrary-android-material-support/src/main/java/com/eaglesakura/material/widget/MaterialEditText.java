28127f2ec59c45a750ea791ea30d7230a653fb48
eaglesakura <eagle.sakura@gmail.com>
2015-04-22 16:31:42 +0900

    EditText wrapperを追加

---
+++ eglibrary-android-material-support/src/main/java/com/eaglesakura/material/widget/MaterialEditText.java
@@ -0,0 +1,22 @@
+package com.eaglesakura.material.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+/**
+ * EditText Wrapper
+ */
+public class MaterialEditText extends AppCompatEditText {
+    public MaterialEditText(Context context) {
+        super(context);
+    }
+
+    public MaterialEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MaterialEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
