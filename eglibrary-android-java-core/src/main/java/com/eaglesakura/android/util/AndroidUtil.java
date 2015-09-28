9fcc7504784a249bcff610deeb6efae4f2dc4235
eaglesakura <eagle.sakura@gmail.com>
2015-09-28 23:06:21 +0900

    Foregroundアプリ取得でLolipop5.1に対応

--- eglibrary-android-java-core/src/main/java/com/eaglesakura/android/util/AndroidUtil.java
+++ eglibrary-android-java-core/src/main/java/com/eaglesakura/android/util/AndroidUtil.java
@@ -1,6 +1,11 @@
 package com.eaglesakura.android.util;
 
+import android.annotation.SuppressLint;
 import android.app.ActivityManager;
+import android.app.AppOpsManager;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -18,7 +23,12 @@ import android.os.Looper;
 import android.os.Vibrator;
 import android.view.TextureView;
 
+import com.eaglesakura.util.StringUtil;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 
 public class AndroidUtil {
 
@@ -48,7 +58,6 @@ public class AndroidUtil {
      * Handlerに関連付けられていたThreadで動作している場合はtrueを返す。
      *
      * @param handler 確認対象のHandler
-     *
      * @return Handlerに関連付けられていたThreadで動作している場合はtrueを返す。
      */
     public static boolean isHandlerThread(Handler handler) {
@@ -79,7 +88,6 @@ public class AndroidUtil {
      * 自分自身がTop Applicationとして起動している場合はtrue
      *
      * @param context
-     *
      * @return
      */
     public static boolean isTopApplicationSelf(Context context) {
@@ -90,7 +98,6 @@ public class AndroidUtil {
      * ランチャー一覧を取得する
      *
      * @param context
-     *
      * @return
      */
     public static List<ResolveInfo> listLauncherApplications(Context context) {
@@ -104,7 +111,6 @@ public class AndroidUtil {
      * インストールされているアプリのpackage名一覧を取得する
      *
      * @param context
-     *
      * @return
      */
     public static List<ApplicationInfo> listInstallApplications(Context context) {
@@ -113,22 +119,56 @@ public class AndroidUtil {
         return infos;
     }
 
+    @SuppressLint("NewApi")
+    public static boolean isUsageStatsAllowed(Context context) {
+        if (Build.VERSION.SDK_INT < 19) {
+            return true;
+        }
+
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        int uid = android.os.Process.myUid();
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, uid, context.getPackageName());
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+
     /**
      * トップに起動しているActivityのpackage nameを指定する
      *
      * @param context
-     *
      * @return
      */
     public static String getTopApplicationPackage(Context context) {
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
-        for (ActivityManager.RunningAppProcessInfo info : processes) {
-            if (info.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
-                if (info.importanceReasonComponent != null) {
-                    return info.importanceReasonComponent.getPackageName();
-                } else {
-                    return info.pkgList[0];
+
+        if (Build.VERSION.SDK_INT >= 22) {
+            UsageStatsManager usm = (UsageStatsManager) context.getSystemService("usagestats");
+            long time = System.currentTimeMillis();
+            UsageEvents events = usm.queryEvents(time - (1000 * 60 * 60), time);
+            if (events != null && events.hasNextEvent()) {
+                UsageEvents.Event app = new android.app.usage.UsageEvents.Event();
+                long lastAppTime = 0;
+                String packageName = null;
+                while (events.hasNextEvent()) {
+                    events.getNextEvent(app);
+                    if (app.getTimeStamp() > lastAppTime && app.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+                        packageName = app.getPackageName();
+                        lastAppTime = app.getTimeStamp();
+                    }
+                }
+
+                if (!StringUtil.isEmpty(packageName)) {
+                    return packageName;
+                }
+            }
+        } else {
+            ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
+            for (ActivityManager.RunningAppProcessInfo info : processes) {
+                if (info.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                    if (info.importanceReasonComponent != null) {
+                        return info.importanceReasonComponent.getPackageName();
+                    } else {
+                        return info.pkgList[0];
+                    }
                 }
             }
         }
